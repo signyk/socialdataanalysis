95072945c5b298374b62f75985e4a7f696ccc9a4
Signý <signy@signys-mbp.students.clients.local>
2023-04-18 13:26:10 +0200

    clean functions

--- src/signy.py
+++ src/signy.py
@@ -6,7 +6,11 @@ from bokeh.plotting import figure, output_file, save
 import folium
 import plotly.express as px
 
-from utils.make_data import get_data
+from utils.make_data import get_data, clean_data
 
 """ Importing data """
-dat = get_data()
+dat_raw = get_data()
+dat = clean_data(dat_raw)
+
+# print the names of the columns of the data
+print(dat.columns)
--- utils/make_data.py
+++ utils/make_data.py
@@ -65,3 +65,88 @@ def get_data():
     ]
     dat = pd.read_csv("data/fireIncidents_clean.csv", parse_dates=parse_dates)
     return dat
+
+
+def clean_data(dat: pd.DataFrame) -> pd.DataFrame:
+    "Dropping columns"
+    # Drop the columns that are not needed
+    dat_clean = dat.drop(
+        [
+            "Box",
+            "Original Priority",
+            "Priority",
+            "Final Priority",
+            "Zipcode of Incident",
+            "Fire Prevention District",
+            "Supervisor District",
+            "Analysis Neighborhoods",
+            "City",
+        ],
+        axis=1,
+    )
+    "Clean up the location column"
+    # extract the latitude and longitude from the case_location column and add them as seperate columns
+    location = dat_clean["case_location"].str.extract(r"\((.+)\)")
+    dat_clean["latitude"] = location[0].str.split(" ").str[0]
+    dat_clean["longitude"] = location[0].str.split(" ").str[1]
+    # convert the latitude and longitude columns to numeric
+    dat_clean["latitude"] = pd.to_numeric(dat_clean["latitude"])
+    dat_clean["longitude"] = pd.to_numeric(dat_clean["longitude"])
+    # drop the case_location column
+    dat_clean = dat_clean.drop(columns=["case_location"])
+
+    "Rename columns"
+    dat_clean = dat_clean.rename(
+        columns={
+            "Call Number": "call_number",
+            "Unit ID": "unit_id",
+            "Incident Number": "incident_number",
+            "Call Type": "call_type",
+            "Call Date": "call_date",
+            "Watch Date": "watch_date",
+            "Received DtTm": "received_dttm",
+            "Entry DtTm": "entry_dttm",
+            "Dispatch DtTm": "dispatch_dttm",
+            "Response DtTm": "response_dttm",
+            "On Scene DtTm": "on_scene_dttm",
+            "Transport DtTm": "transport_dttm",
+            "Hospital DtTm": "hospital_dttm",
+            "Call Final Disposition": "call_final_disposition",
+            "Available DtTm": "available_dttm",
+            "Address": "address",
+            "Battalion": "battalion",
+            "Station Area": "station_area",
+            "ALS Unit": "als_unit",
+            "Call Type Group": "call_type_group",
+            "Number of Alarms": "number_of_alarms",
+            "Unit Type": "unit_type",
+            "Unit sequence in call dispatch": "unit_sequence",
+            "Neighborhooods - Analysis Boundaries": "neighborhood",
+            "RowID": "row_id",
+            "latitude": "latitude",
+            "longitude": "longitude",
+        }
+    )
+
+    "Drop rows"
+    # Drop the rows with call_final_disposition == "Cancelled" or "Duplicate"
+    dat_clean = dat_clean.loc[
+        (dat_clean["call_final_disposition"] != "Cancelled")
+        & (dat_clean["call_final_disposition"] != "Duplicate")
+    ]
+
+    # Drop the rows with on_scene_dttm == NaT
+    dat_clean = dat_clean.loc[dat_clean["on_scene_dttm"].notna()]
+
+    return dat_clean
+
+
+def get_on_scene_map(dat: pd.DataFrame) -> pd.DataFrame:
+    "Map incident_number to on_scene_time"
+    # Create a column for the on scene time in minutes
+    dat["on_scene_time"] = (
+        dat["on_scene_dttm"] - dat["received_dttm"]
+    ).dt.total_seconds() / 60
+
+    # Drop rows where on_scene_time < 0 (these are errors, AM/PM confusion)
+    dat = dat.loc[dat["on_scene_time"] >= 0]
