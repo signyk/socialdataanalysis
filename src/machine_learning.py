1d4959671009b2486a767e691d124333cecdf306
Signý <signy@Signys-MacBook-Pro.local>
2023-05-08 00:59:18 +0200

    more ML

--- src/machine_learning.py
+++ src/machine_learning.py
@@ -1,7 +1,6 @@
 """ Importing packages """
-import plotly.express as px
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Machine learning
 from sklearn.model_selection import train_test_split
@@ -13,21 +12,13 @@ from sklearn.linear_model import LinearRegression
 
 # Bokeh
 from bokeh.plotting import show, figure
-from bokeh.embed import components
-from bokeh.models import ColumnDataSource, HoverTool, DatetimeTickFormatter
 from bokeh.io import show, output_file
+from bokeh.models import ColumnDataSource, HoverTool
 
 # Local
-from utils.make_data import get_data, clean_data, get_neighborhoods, filter_data_years
-from utils.const import FILTER_CALL_TYPES
-from utils.plot_functions import (
-    make_bokeh_line_plot,
-    make_map,
-    make_bokeh_tabs,
-    make_cal_plot,
-    plot_importance,
-)
-from utils.help_functions import get_viridis_pallette, format_string
+from utils.make_data import get_data, clean_data, filter_data_years
+from utils.plot_functions import plot_importance
+from utils.help_functions import get_viridis_pallette
 
 """ Importing data """
 dat_raw = get_data()
@@ -48,7 +39,6 @@ column_names = X_dat.columns
 # Scale the data
 scaler = StandardScaler()
 X_dat = scaler.fit_transform(X_dat)
-
 # Create traning and test data
 y_train, y_test, X_train, X_test = train_test_split(
     y, X_dat, test_size=0.2, random_state=1
@@ -65,30 +55,28 @@ y_pred = model.predict(X_test)
 mean_squared_error(y_test, y_pred)
 
 # Get the feature importances
-importances = model.feature_importances_
+importances_rf = model.feature_importances_
 
-p = plot_importance(column_names, importances)
+# Plot the feature importances
+indices_rf = importances_rf.argsort()[-10:][::-1]
+p = plot_importance(column_names[indices_rf], importances_rf[indices_rf])
 output_file("figs/feature_importance_random_forest.html")
 show(p)
-
-# Get the indices of the most important features
-indices = importances.argsort()[-5:][::-1]
-
-# Get the names of the most important features
-names = [column_names[i] for i in indices]
-
 
 """ Tree-based feature selection """
 # Create an instance of ExtraTreesRegressor
 et = ExtraTreesRegressor(n_estimators=100)
 
 # Fit ExtraTreesRegressor on the training data
-et.fit(X_train, y_train)
+et.fit(X_dat, y)
 
-# Print the feature importances
-print(et.feature_importances_)
+# Get the feature importances
+importances_et = et.feature_importances_
 
-p = plot_importance(column_names, et.feature_importances_)
+# Plot the feature importances
+indices_et = importances_et.argsort()[-10:][::-1]
+
+p = plot_importance(column_names[indices_et], importances_et[indices_et])
 output_file("figs/feature_importance_tree_based.html")
 show(p)
 
@@ -101,7 +89,75 @@ lr = LinearRegression()
 rfe = RFE(lr, n_features_to_select=5)
 
 # Fit RFE on the training data
-rfe.fit(X_train, y_train)
+rfe.fit(X_dat, y)
 
 # Print the most important features
 print(column_names[rfe.support_])
+
+# Get the feature importances
+importances_rfe = rfe.ranking_
+
+# Get the 10 most important features
+indices_rfe = importances_rfe.argsort()[:10]
+
+
+""" Summary """
+# Data frame the the top 10 features according to different methods
+top_10_features = pd.DataFrame(
+    {
+        "Random Forest": column_names[indices_rf],
+        "Tree-based feature selection": column_names[indices_et],
+        "Recursive feature elimination": column_names[indices_rfe],
+        "Importance": [i for i in range(10, 0, -1)],
+    }
+)
+
+# Transform the data frame to long format
+top_10_features_long = pd.melt(
+    top_10_features, id_vars=["Importance"], var_name="Method", value_name="Feature"
+)
+
+processed_dat = top_10_features_long.pivot(
+    index="Feature", columns="Method", values="Importance"
+).reset_index()
+processed_dat = processed_dat.fillna(0)
+
+src = ColumnDataSource(processed_dat)
+
+# Create a figure
+p = figure(
+    x_range=top_10_features_long["Feature"].unique(),
+    height=500,
+    width=800,
+    title="Top 10 features",
+    toolbar_location=None,
+    tools="hover",
+    tooltips=[
+        ("Method", "$name"),
+        ("Feature", "@Feature"),
+        ("Importance", "@$name"),
+    ],
+)
+
+# Add stacked bars to the figure for each method
+p.vbar_stack(
+    top_10_features_long["Method"].unique(),
+    x="Feature",
+    width=0.9,
+    source=src,
+    color=get_viridis_pallette(3),
+    legend_label=[
+        "Random Forest",
+        "Tree-based feature selection",
+        "Recursive feature elimination",
+    ],
+)
+
+p.y_range.start = 0
+p.xgrid.grid_line_color = None
+p.axis.minor_tick_line_color = None
+p.outline_line_color = None
+p.legend.location = "top_right"
+p.xaxis.major_label_orientation = 1.2
+
+show(p)
